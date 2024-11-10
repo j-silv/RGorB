4d4e172322e6da0056bbd0456d4f2d7edb9c5436
Justin Silver <justinrsilver@gmail.com>
2024-11-10 09:21:15 -0800

    Remove clock function since internal clock is selected by default on STM32

--- src/main.c
+++ src/main.c
@@ -17,18 +17,12 @@
 #include "bno055.h"
 #include "i2c.h"
 
+volatile uint32_t msTicks; /* counts 1ms timeTicks from SysTickHandler */  
 
-volatile uint32_t msTicks;                            /* counts 1ms timeTicks */
-/*----------------------------------------------------------------------------
- * SysTick_Handler:
- *----------------------------------------------------------------------------*/
 void SysTick_Handler(void) {
   msTicks++;
 }
 
-/*----------------------------------------------------------------------------
- * Delay: delays a number of Systicks
- *----------------------------------------------------------------------------*/
 void Delay (uint32_t dlyTicks) {
   uint32_t curTicks;
 
@@ -36,76 +30,40 @@ void Delay (uint32_t dlyTicks) {
   while ((msTicks - curTicks) < dlyTicks) { __NOP(); }
 }
 
-/*----------------------------------------------------------------------------
- * SystemCoreClockConfigure: configure SystemCoreClock using HSI
-                             (HSE is not populated on Nucleo board)
- *----------------------------------------------------------------------------*/
-void SystemCoreClockConfigure(void) {
-
-  RCC->CR |= ((uint32_t)RCC_CR_HSION);                     /* Enable HSI */
-  while ((RCC->CR & RCC_CR_HSIRDY) == 0);                  /* Wait for HSI Ready */
-
-  RCC->CFGR = RCC_CFGR_SW_HSI;                             /* HSI is system clock */
-  while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_HSI);  /* Wait for HSI used as system clock */
-
-  FLASH->ACR  = (FLASH_ACR_PRFTEN     |                    /* Enable Prefetch Buffer */
-                 FLASH_ACR_ICEN       |                    /* Instruction cache enable */
-                 FLASH_ACR_DCEN       |                    /* Data cache enable */
-                 FLASH_ACR_LATENCY_5WS );                  /* Flash 5 wait state */
-
-  RCC->CFGR |= (RCC_CFGR_HPRE_DIV1  |                      /* HCLK = SYSCLK */
-                RCC_CFGR_PPRE1_DIV2 |                      /* APB1 = HCLK/2 */
-                RCC_CFGR_PPRE2_DIV1  );                    /* APB2 = HCLK/1 */
-
-  RCC->CR &= ~RCC_CR_PLLON;                                /* Disable PLL */
-
-  /* PLL configuration:  VCO = HSI/M * N,  Sysclk = VCO/P */
-  RCC->PLLCFGR = ( 16ul                   |                /* PLL_M =  16 */
-                 (200ul <<  6)            |                /* PLL_N = 200 */
-                 (  0ul << 16)            |                /* PLL_P =   2 */
-                 (RCC_PLLCFGR_PLLSRC_HSI) |                /* PLL_SRC = HSI */
-                 (  7ul << 24)            |                /* PLL_Q =   7 */
-                 (  2ul << 28)             );              /* PLL_R =   2 */
-
-  RCC->CR |= RCC_CR_PLLON;                                 /* Enable PLL */
-  while((RCC->CR & RCC_CR_PLLRDY) == 0) __NOP();           /* Wait till PLL is ready */
-
-  RCC->CFGR &= ~RCC_CFGR_SW;                               /* Select PLL as system clock source */
-  RCC->CFGR |=  RCC_CFGR_SW_PLL;
-  while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL);  /* Wait till PLL is system clock src */
-}
-
-/*----------------------------------------------------------------------------
- * main: blink LED
- *----------------------------------------------------------------------------*/
 int main (void) {
-  SystemCoreClockConfigure();                              /* configure HSI as System Clock */
-  SystemCoreClockUpdate();
-  SysTick_Config(SystemCoreClock / 1000);                  /* SysTick 1 msec interrupts */
+  SysTick_Config(SystemCoreClock / 1000); /* SysTick 1 msec interrupts */                  
 
   RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN; // enable clock for GPIOA
   GPIOA->MODER |= GPIO_MODER_MODER5_0;  // set as GPIO output
 
-  //##############################
-  // needed to reset BNO
-  GPIOA->MODER |= GPIO_MODER_MODER6_0;
-  GPIOA->ODR &= ~GPIO_ODR_OD6;
-  Delay(500);
-  GPIOA->ODR |= GPIO_ODR_OD6;
-  Delay(500);
-  //##############################
+  // //##############################
+  // // needed to reset BNO
+  // GPIOA->MODER |= GPIO_MODER_MODER6_0;
+  // GPIOA->ODR &= ~GPIO_ODR_OD6;
+  // Delay(500);
+  // GPIOA->ODR |= GPIO_ODR_OD6;
+  // Delay(500);
+  // //##############################
 
-  I2C_INIT();
-  uint8_t return_code = init_bno055();
-  if (return_code == NO_SYSTEM_ERROR) {
-    GPIOA->ODR |= GPIO_ODR_OD5; // turn on GPIOA_P5 (LED)
-  }
+  // I2C_INIT();
+  // uint8_t return_code = init_bno055();
+  // if (return_code == NO_SYSTEM_ERROR) {
+  //   GPIOA->ODR |= GPIO_ODR_OD5; // turn on GPIOA_P5 (LED)
+  // }
   
-  int temp =  0;
-  double acc = 0;
-  while(1){
-      temp = convert_temp(get_temp());
-      acc = convert_acc(get_acc_x_data());
+  // int temp =  0;
+  // double acc = 0;
+  // while(1){
+  //     temp = convert_temp(get_temp());
+  //     acc = convert_acc(get_acc_x_data());
+  // }
+
+
+  while(1) {
+    GPIOA->ODR |= GPIO_ODR_OD5; // turn on GPIOA_P5 (LED)
+    Delay(1000);
+    GPIOA->ODR &= ~GPIO_ODR_OD5; // turn on GPIOA_P5 (LED)
+    Delay(1000);
   }
 }
 
